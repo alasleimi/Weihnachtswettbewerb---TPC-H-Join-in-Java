aa6eb5c5591b2bc830b0cdd71b2eb6f30f9c4444
ge35gil <a.sleimi@tum.de>
2019-12-24 23:48:10 +0100

    try

--- src/pgdp/freiwillig/Database.java
+++ src/pgdp/freiwillig/Database.java
@@ -31,8 +31,9 @@ public class Database {
 
     public static Stream<Customer> processInputFileCustomer() {
         try {
-            return Files.lines(baseDataDirectory.resolve("customer.tbl")).
-                    map(x -> {
+            return Files.lines(baseDataDirectory.resolve("customer.tbl"))
+                    .parallel()
+                    .map(x -> {
                         String[] d = x.split("\\|");
 
 
@@ -53,18 +54,19 @@ public class Database {
 
     public static Stream<LineItem> processInputFileLineItem() {
         try {
-            return Files.lines(baseDataDirectory.resolve("lineitem.tbl")).
-                    map(x -> {
-                                String[] d = x.split("\\|");
+            return Files.lines(baseDataDirectory.resolve("lineitem.tbl"))
+                    .parallel()
+                    .map(x -> {
+                        String[] d = x.split("\\|");
 
 
-                                return new LineItem(Integer.parseInt(d[0]),
-                                        Integer.parseInt(d[1]), Integer.parseInt(d[2]), Integer.parseInt(d[3]),
-                                        Integer.parseInt(d[4]) * 100,
-                                        Float.parseFloat(d[5]),
-                                        Float.parseFloat(d[6]), Float.parseFloat(d[7]), d[8].charAt(0),
-                                        d[9].charAt(0), LocalDate.parse(d[10]),
-                                        LocalDate.parse(d[11]), LocalDate.parse(d[12]), d[13].toCharArray(),
+                        return new LineItem(Integer.parseInt(d[0]),
+                                Integer.parseInt(d[1]), Integer.parseInt(d[2]), Integer.parseInt(d[3]),
+                                Integer.parseInt(d[4]) * 100,
+                                Float.parseFloat(d[5]),
+                                Float.parseFloat(d[6]), Float.parseFloat(d[7]), d[8].charAt(0),
+                                d[9].charAt(0), LocalDate.parse(d[10]),
+                                LocalDate.parse(d[11]), LocalDate.parse(d[12]), d[13].toCharArray(),
                                         d[14].toCharArray(), d[15].toCharArray()
                                 );
                             }
@@ -80,7 +82,8 @@ public class Database {
     public static Stream<Order> processInputFileOrders() {
         try {
             return Files.lines(baseDataDirectory.resolve("orders.tbl")).
-                    map(x -> {
+                    parallel()
+                    .map(x -> {
                         String[] d = x.split("\\|");
 
                         return new Order(Integer.parseInt(d[0]), Integer.parseInt(d[1].replaceAll("\\D", ""))
@@ -107,18 +110,18 @@ public class Database {
     public long getAverageQuantityPerMarketSegment(String marketsegment) {
         if (!cache) {
             var mktsegmentPerCustomer = processInputFileCustomer().collect(
-                    Collectors.groupingBy(x -> x.custKey, Collectors.mapping(x -> x.mktsegment, Collectors.joining())
+                    Collectors.groupingByConcurrent(x -> x.custKey, Collectors.mapping(x -> x.mktsegment, Collectors.joining())
                     ));
 
             var mktsegmentPerOrder = processInputFileOrders().collect(
-                    Collectors.groupingBy(x -> x.orderKey,
+                    Collectors.groupingByConcurrent(x -> x.orderKey,
                             Collectors.mapping(x -> mktsegmentPerCustomer.get(x.custKey),
                                     Collectors.joining())
                     )
             );
 
             averageQuantityPerMarketSegment = processInputFileLineItem()
-                    .collect(Collectors.groupingBy(x -> mktsegmentPerOrder.get(x.orderKey),
+                    .collect(Collectors.groupingByConcurrent(x -> mktsegmentPerOrder.get(x.orderKey),
                             Collectors.teeing(
                                     Collectors.summingLong(x -> x.quantity),
                                     Collectors.counting(),
