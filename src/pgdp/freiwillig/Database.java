6c06a9d80c11293581fb69a61691c4de6bbb4e08
ge35gil <a.sleimi@tum.de>
2019-12-25 13:54:15 +0100

    try

--- src/pgdp/freiwillig/Database.java
+++ src/pgdp/freiwillig/Database.java
@@ -31,24 +31,6 @@ public class Database {
 
     }
 
-
-    public static Map<Integer, Integer> custPerOrder() {
-        try {
-            return Files.lines(baseDataDirectory.resolve("orders.tbl"))
-                    .unordered()
-                    .parallel()
-                    .map(x -> x.split("\\|"))
-                    .collect(Collectors.toConcurrentMap(x -> parseInt(x[0]), x -> parseInt(x[1]), (x, v) -> v,
-                            () -> new ConcurrentHashMap<Integer, Integer>(1 << 23)));
-
-
-        } catch (IOException e) {
-
-            throw new RuntimeException("file not found");
-        }
-        // TODO
-    }
-
     public static Map<Integer, String> segPerCust() {
         try {
             return Files.lines(baseDataDirectory.resolve("customer.tbl"))
@@ -59,8 +41,8 @@ public class Database {
                         int j = 0;
                         int o = 0;
                         for (int i = 0; i < x.length(); i++) {
-                            char c = x.charAt(i);
-                            if (c == '|') {
+
+                            if (x.charAt(i) == '|') {
                                 if (j == 1) {
                                     answer[0] = x.substring(o, i);
                                 } else if (j == 6) {
@@ -77,8 +59,8 @@ public class Database {
                         //System.out.println(answer[0] + " " + answer[1]);
                         return answer;
                     })
-                    .collect(Collectors.toConcurrentMap(x -> parseInt(x[0]), x -> x[1], (x, v) -> v,
-                            () -> new ConcurrentHashMap<Integer, String>(1 << 23)));
+                    .collect(Collectors.toConcurrentMap(x -> parseInt(x[0], 0, x[0].length()), x -> x[1], (x, v) -> v,
+                            () -> new ConcurrentHashMap<Integer, String>(1 << 24)));
 
 
         } catch (IOException e) {
@@ -104,51 +86,41 @@ public class Database {
         System.out.println((e - s) / 1_000_000);
     }
 
-    static int parseInt(String s) {
-        int i = 0;
+    static int parseInt(String s, int start, int end) {
+        int i = start;
         int q = 0;
-        char c;
-        for (c = s.charAt(i); c > '9' || c < '1'; c = s.charAt(++i)) ;
-        for (; i < s.length(); ++i) {
-            c = s.charAt(i);
-            q *= 10;
-            q += c - '0';
 
+        for (char c = s.charAt(i); c > '9' || c < '1'; c = s.charAt(++i)) ;
+        for (; i < end; ++i) {
+            q *= 10;
+            q += s.charAt(i) - '0';
         }
         return q;
-
-
     }
 
     public long getAverageQuantityPerMarketSegment(String marketsegment) {
         if (!cache) {
             //var a = custPerOrder();
             //System.out.println(a.size());
-            var b = segPerCust();
+            var sPC = segPerCust();
             // System.out.println(b.size());
             try {
+
 
                 var a = Files.lines(baseDataDirectory.resolve("orders.tbl"))
                         .unordered()
                         .parallel()
-                        .map(x -> x.split("\\|"))
-                        .collect(Collectors.toConcurrentMap(x -> parseInt(x[0]), x -> b.get(parseInt(x[1])), (x, v) -> v,
-                                () -> new ConcurrentHashMap<Integer, String>(1 << 23)));
-
-                averageQuantityPerMarketSegment = Files.lines(baseDataDirectory.resolve("lineitem.tbl"))
-                        .unordered()
-                        .parallel()
                         .map(x -> {
-                            String[] answer = new String[2];
+                            int[] answer = new int[2];
                             int j = 0;
                             int o = 0;
                             for (int i = 0, n = x.length(); i < n; i++) {
 
                                 if (x.charAt(i) == '|') {
                                     if (j == 0) {
-                                        answer[0] = x.substring(o, i);
-                                    } else if (j == 4) {
-                                        answer[1] = x.substring(o, i);
+                                        answer[0] = parseInt(x, o, i);
+                                    } else if (j == 1) {
+                                        answer[1] = parseInt(x, o, i);
                                         break;
                                     }
                                     o = i + 1;
@@ -160,14 +132,48 @@ public class Database {
                             }
                             //System.out.println(answer[0] + " " + answer[1]);
                             return answer;
-                        }).collect(Collectors.groupingByConcurrent(x -> a.get(parseInt(x[0])),
+
+
+                        })
+                        .collect(Collectors.toConcurrentMap(x -> x[0], x -> sPC.get(x[1]), (x, v) -> v,
+                                () -> new ConcurrentHashMap<Integer, String>(1 << 24)));
+
+
+                averageQuantityPerMarketSegment = Files.lines(baseDataDirectory.resolve("lineitem.tbl"))
+                        .unordered()
+                        .parallel()
+                        .map(x -> {
+                            //custom split
+                            int[] answer = new int[2];
+                            int j = 0;
+                            int o = 0;
+                            for (int i = 0, n = x.length(); i < n; i++) {
+
+                                if (x.charAt(i) == '|') {
+                                    if (j == 0) {
+                                        answer[0] = parseInt(x, o, i);
+                                    } else if (j == 4) {
+                                        answer[1] = parseInt(x, o, i);
+                                        break;
+                                    }
+                                    o = i + 1;
+                                    ++j;
+
+                                }
+
+
+                            }
+                            //System.out.println(answer[0] + " " + answer[1]);
+                            return answer;
+                        }).collect(Collectors.groupingByConcurrent(x -> a.get(x[0]),
                                 () -> new ConcurrentHashMap<>(1_000_000),
                                 Collectors.teeing(
-                                        Collectors.summingLong(x -> parseInt(x[1])),
+                                        Collectors.summingLong(x -> x[1]),
                                         Collectors.counting(),
                                         (s, c) -> (100 * s) / c
 
                                 )
+
                         ));
             } catch (IOException e) {
                 throw new RuntimeException("no file");
