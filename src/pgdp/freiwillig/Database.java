0dead7ecae384bbb65d88f5f89672e99a0a44e5b
ge35gil <a.sleimi@tum.de>
2019-12-28 12:39:58 +0100

    Revert "remove parseInt"
    This reverts commit ccceb2f28e705193203ff6dc3318f2ae502b0b6e.

--- src/pgdp/freiwillig/Database.java
+++ src/pgdp/freiwillig/Database.java
@@ -125,6 +125,17 @@ public class Database {
     }
 
 
+    static int parseInt(long i, long end) {
+
+        int q = 0;
+
+        for (byte c = unsafe.getByte(i); c > '9' || c < '1'; c = unsafe.getByte(++i)) ;
+        for (; i < end; ++i) {
+            q *= 10;
+            q += unsafe.getByte(i) - '0'; // - 0
+        }
+        return q;
+    }
 
     PairArr getOrderToSegment(PairArr customerToSegment, MappedByteBuffer orders) {
 
