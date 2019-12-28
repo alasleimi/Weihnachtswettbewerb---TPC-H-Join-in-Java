4cf5bc7f4ecb20299c332c9afd3a9d58b9805c19
ge35gil <a.sleimi@tum.de>
2019-12-28 19:59:15 +0100

    no pre-load

--- src/pgdp/freiwillig/Database.java
+++ src/pgdp/freiwillig/Database.java
@@ -264,7 +264,7 @@ public class Database {
             var ch = new FileInputStream(baseDataDirectory.resolve(tableName).toFile()).getChannel();
             //buf.load(); ???
             var buff = ch.map(FileChannel.MapMode.READ_ONLY, 0, ch.size());
-            buff.load();
+            //buff.load(); pre-load or not?
             return buff;
 
         };
